cdc003ba75cef3fdfb6dc9bb954d624f4eb6ad09
chendaile <3185854290@qq.com>
2025-12-25 01:34:59 +0800

    feat:PRINT-FUCK-HTTPserver

--- SimpleHTTP.java
+++ SimpleHTTP.java
@@ -11,22 +11,32 @@ public class SimpleHTTP {
 	InetSocketAddress addr = new InetSocketAddress(8000);
 	HttpServer server = HttpServer.create(addr, 0);
 
-	HttpHandler handler = new HttpHandler() {
-		@Override
-	        public void handle (HttpExchange exchange) throws IOException{
-		    String response = "Hello World";
-		    exchange.sendResponseHeaders(200, response.length());
-		    try(OutputStream os = exchange.getResponseBody()) {
-			os.write(response.getBytes());
-		    }
-		}
-	};
-	server.createContext("/", handler);
+	SimpleHandler handler01 = new SimpleHandler("nihao");
+	SimpleHandler handler02 = new SimpleHandler("FUCKYOU");
+	server.createContext("/nm/", handler02);
+	server.createContext("/", handler01);
 
 	server.setExecutor(null);
 	server.start();
 
 	System.out.println("Server is running!");
+    }
+
+    public static class SimpleHandler implements HttpHandler {
+	public String responseText;
+
+	public SimpleHandler(String responseText) {
+	    this.responseText = responseText;
+	}
+	
+	@Override
+	public void handle (HttpExchange exchange) throws IOException{
+	    byte[] responseBytes = responseText.getBytes();
+	    exchange.sendResponseHeaders(200, responseBytes.length);
+	    try(OutputStream os = exchange.getResponseBody()) {
+		os.write(responseBytes);
+	    }
+	}
     }
 }
 
